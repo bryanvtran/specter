74aab76ccafb7a5463e06fb0835d858da5f63975
Bryan Tran <bryanvantran@ucla.edu>
2019-01-24 23:46:22 -0800

    js and button css

--- static/js/main.js
+++ static/js/main.js
@@ -5,4 +5,34 @@ document.addEventListener('DOMContentLoaded', function(){
         e.preventDefault();
         console.log('form submitted');
     });
+
+    // scroll
+    const elements = document.querySelectorAll('.scroll');
+    elements.forEach(function(element) {
+        element.addEventListener('click', function(e) {
+            // Make sure this.hash has a value before overriding default behavior
+            if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                e.preventDefault();
+
+                // Store hash
+                const hash = this.hash;
+
+                // use scroll into view
+                document.querySelector(hash).scrollIntoView({ 
+                    behavior: 'smooth',
+                    block: 'start',
+                    inline: 'nearest'
+                    
+                });
+
+                // focus on input
+                document.querySelector('#mce-EMAIL').focus();
+                
+                // Add hash (#) to URL when done scrolling (default click behavior)
+                window.location.hash = hash;
+            }
+        });
+    })
+
 });
